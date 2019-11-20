9c6a59af0d33252fc57eac79a2545f2d3216c76d
Charlie Young <173063877@qq.com>
2019-11-20 11:32:06 +0800

    修改Branch2代码

--- app/src/main/java/com/example/coolweather/MainActivity.java
+++ app/src/main/java/com/example/coolweather/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //修改Branch1代码
+        //修改Branch2代码
     }
 }
